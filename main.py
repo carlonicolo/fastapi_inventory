7bab9be7fd568e4fc0d31d0560b4008c59ec7e78
Carlo Nicolò <carlo.nicolo@hotmail.com>
2022-04-02 01:20:46 +0200

    Completed first simple microservice with redis database

--- main.py
+++ main.py
@@ -1,17 +1,67 @@
 from fastapi import FastAPI
-from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
 import config
 
 app = FastAPI()
+
+#Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 # Connect to redis
 redis = get_redis_connection(
     host=config.host,
     port=config.port,
-    password=config.password
+    password=config.password,
+    decode_responses=config.decode_responses
 )
 
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
 
+    class Meta:
+        database = redis
+
+
+@app.get('/products')
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
+
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
+
+
+'''
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+'''
