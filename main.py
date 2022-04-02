9caaa9763f4ce981253aee8aeba30ca663c99ab0
Carlo Nicolò <carlo.nicolo@hotmail.com>
2022-04-02 11:40:55 +0200

    fixez

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 #Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['http://localhost:3000','http://127.0.0.1/8001'],
     allow_methods=['*'],
     allow_headers=['*']
 )
